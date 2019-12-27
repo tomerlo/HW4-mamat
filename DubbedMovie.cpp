6ea1f85de7f87a0dea59e064336eb4fef941f838
Yaniv Shlomovich <yanivsh@campus.technion.ac.il>
2019-12-27 17:38:41 +0200

    fix

--- DubbedMovie.cpp
+++ DubbedMovie.cpp
@@ -23,22 +23,14 @@ DubbedMovie::DubbedMovie(string movieName, int movieLength, string movieLanguage
 //*****************************************************************************************************
 BOOL DubbedMovie::addHebrewScreening(int day, int screeningTime)
 {
+
 	if (day < 1 || screeningTime < 1 || screeningTime>24) {
 		return FALSE;
 	}
 	if (m_streamingTimesMat[day - 1][MAX_SCREENINGS_PER_DAY - 1] != 0) {
 		return FALSE; //because it means the day is full of screenings
 	}
-	int j = 0;
-	for (; j < MAX_SCREENINGS_PER_DAY && m_streamingTimesMat[day - 1][j] != 0; j++) {//check if
-		//there is a screening in the same day which haven't finished yet
-		if (m_streamingTimesMat[day - 1][j] + m_movieLength > screeningTime) { //found a movie which haven't finished yet 
-			return FALSE;
-		}
-	}
-	//if we reached here it means we found an empty place and all previous screenings have finished already
-	m_streamingTimesMat[day - 1][j] = screeningTime; //put it in the mat
-	return TRUE;
+	addScreening(day, screeningTime);
 };
 
 
@@ -55,23 +47,7 @@ int DubbedMovie::getNextHebrewScreening(int day, int time) const
 	if (day < 1 || screeningTime < 1 || screeningTime>24) {
 		return 0;
 	}
-	int j = 0;
-	for (; j < MAX_SCREENINGS_PER_DAY; j++) {
-		if (m_streamingTimesMat[day - 1][j] != 0 &&
-			(m_streamingTimesMat[day - 1][j] < time || m_streamingTimesMat[day - 1][j] + m_movieLength < time)) {
-			continue; //current screening was started or finished already
-		}
-		break; //found a screening which haven't started yet or reached a 0 in m_streamingTimesMat
-	}
-	if (j == MAX_SCREENINGS_PER_DAY) {//means all movies were started or finished before 'time'
-		return 0;
-	}
-	else if (m_streamingTimesMat[day - 1][j] == 0) {//means all screening times available are in the past
-		return 0;
-	}
-	else {
-		return m_streamingTimesMat[day - 1][j]; //valid next screening time
-	}
+	getNextScreening(day, time);
 };
 
 
