c04743650ba46da96d06006fb452b6a1f168b0a3
Yaniv Shlomovich <yanivsh@campus.technion.ac.il>
2019-12-27 19:11:00 +0200

    fix screening

--- DubbedMovie.cpp
+++ DubbedMovie.cpp
@@ -29,14 +29,22 @@ DubbedMovie::DubbedMovie(string movieName, int movieLength, string movieLanguage
 //*****************************************************************************************************
 BOOL DubbedMovie::addHebrewScreening(int day, int screeningTime)
 {
-
 	if (day < 1 || screeningTime < 1 || screeningTime>24) {
 		return FALSE;
 	}
 	if (m_streamingDubbedTimesMat[day - 1][MAX_SCREENINGS_PER_DAY - 1] != 0) {
 		return FALSE; //because it means the day is full of screenings
 	}
-	addScreening(day, screeningTime);
+	int j = 0;
+	for (; j < MAX_SCREENINGS_PER_DAY && m_streamingDubbedTimesMat[day - 1][j] != 0; j++) {//check if
+		//there is a screening in the same day which haven't finished yet
+		if (m_streamingDubbedTimesMat[day - 1][j] + m_movieLength > screeningTime) { //found a movie which haven't finished yet 
+			return FALSE;
+		}
+	}
+	//if we reached here it means we found an empty place and all previous screenings have finished already
+	m_streamingDubbedTimesMat[day - 1][j] = screeningTime; //put it in the mat
+	return TRUE;
 };
 
 
@@ -53,7 +61,23 @@ int DubbedMovie::getNextHebrewScreening(int day, int time) const
 	if (day < 1 || screeningTime < 1 || screeningTime>24) {
 		return 0;
 	}
-	getNextScreening(day, time);
+	int j = 0;
+	for (; j < MAX_SCREENINGS_PER_DAY; j++) {
+		if (m_streamingDubbedTimesMat[day - 1][j] != 0 &&
+			(m_streamingDubbedTimesMat[day - 1][j] < time || m_streamingDubbedTimesMat[day - 1][j] + m_movieLength < time)) {
+			continue; //current screening was started or finished already
+		}
+		break; //found a screening which haven't started yet or reached a 0 in m_streamingTimesMat
+	}
+	if (j == MAX_SCREENINGS_PER_DAY) {//means all movies were started or finished before 'time'
+		return 0;
+	}
+	else if (m_streamingDubbedTimesMat[day - 1][j] == 0) {//means all screening times available are in the past
+		return 0;
+	}
+	else {
+		return m_streamingDubbedTimesMat[day - 1][j]; //valid next screening time
+	}
 };
 
 
