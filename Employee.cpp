d78d36053ce72fa94bc5c2c6108870f54afe2d49
Yaniv Shlomovich <yanivsh@campus.technion.ac.il>
2019-12-30 11:45:17 +0200

    yaniv's 2nd Part

--- Employee.cpp
+++ Employee.cpp
@@ -1,7 +1,62 @@
 #include "Employee.H"
 
-//lemamesh
-Employee(string workerName, int workerSalary, string* workHours);
-getName() const;
-getSalary() const;
-calcWeeklySalary() const;
+
+//*****************************************************************************************************
+//* function name: Employee
+//* Description: Employee constructor
+//* Parameters: workerName - string
+//*				workerSalary - int
+//*				workHours - string*
+//* Return Value:None
+//*****************************************************************************************************
+Employee::Employee(string workerName, int workerSalary, string* workHours) : m_salary(workerSalary)
+{
+	strcpy(m_name, workerName);
+	m_workHours = workHours;
+};
+
+//*****************************************************************************************************
+//* function name: getName
+//* Description: Return the name of the employee
+//* Parameters: None
+//* Return Value: m_name - string
+//*****************************************************************************************************
+
+string Employee::getName() const
+{
+	return m_name;
+}
+
+
+//*****************************************************************************************************
+//* function name: getSalary
+//* Description: Return the salary by hour of the employee
+//* Parameters: None
+//* Return Value: m_salary - int
+//*****************************************************************************************************
+
+int Employee::getSalary() const
+{
+	return m_salary;
+}
+
+//*****************************************************************************************************
+//* function name: calcWeeklySalary
+//* Description: Return employee total salary of the week
+//* Parameters: None
+//* Return Value: Salary - int
+//*****************************************************************************************************
+
+int Employee::calcWeeklySalary() const
+{
+	int Salary=0;
+	int i;
+	for (i = 0; i < 7;i++) {
+		StartHour = strtok(m_workHours[i], "-");
+		EndHour = strtok(NULL, "=");
+		StartHourAsInt = atoi(StartHour);
+		EndHourAsInt = atoi(EndHour);
+		Salary = Salary + (EndHourAsInt-StartHourAsInt)* m_salary
+	}
+	return Salary;
+}
